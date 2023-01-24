750c45a2aff05233caadbbb061b9326c354bd2e1
Ye Myint Soe <52601159+yemyintsoe@users.noreply.github.com>
2023-01-24 08:47:05 +0630

    Create Microsoft-office-activation.js

---
+++ windows/Microsoft-office-activation.js
@@ -0,0 +1,7 @@
+// Fix Product Activation Failed - This Copy Of Microsoft Office Is Not Activated
+
+go to -> C:\Program Files\Microsoft Office\Office16
+then, run this <OSPPREARM.EXE> as Run as Administrator for 3 times
+then it'll work like a cinch
+
+ref: https://youtu.be/gkkp8GCbYyQ
